9f96c2091c14239f34f7125efee322b327a6a245
NathanKun <nathanhejunyang@gmail.com>
2018-11-21 16:06:47 +0100

    Add Auth.java.example

--- src/main/java/com/catprogrammer/jira/constant/package-info.java
+++ src/main/java/com/catprogrammer/jira/constant/package-info.java
@@ -1,13 +1,6 @@
 /**
  * Constants
  * 
- * A class Auth is needed.
- * with
- *  final static String user = "username";
- *  final static String pw = "password";
- *  final static String url = "https://example.com/jira/rest/";
- * And its' getter 
- * 
  * @author NatahanKun
  *
  */
